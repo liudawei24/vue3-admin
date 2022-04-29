a78e25a2491108fc042e2279315af9600ee5555f
刘大卫 <15851990699@163.com>
2022-04-29 11:08:49 +0800

    chore: 添加 pathResolve

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+// import { resolve } from 'path'
+
+// 大概意思 cd __dirname + cd . + cd dir
+// function pathResolve(dir: string) {
+//   return resolve(__dirname, '.', dir)
+// }
 
 // https://vitejs.dev/config/
 export default defineConfig({
