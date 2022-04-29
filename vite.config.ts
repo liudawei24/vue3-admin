6afbbb1262ea7a8f986a44d4ee567519dbbf0472
刘大卫 <15851990699@163.com>
2022-04-29 11:09:58 +0800

    chore: 添加 pathResolve

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 // import { resolve } from 'path'
 
-// 大概意思 cd __dirname + cd . + cd dir
+// 大概意思 cd __dirname(当前模块的目录名) + cd . + cd dir
 // function pathResolve(dir: string) {
 //   return resolve(__dirname, '.', dir)
 // }
