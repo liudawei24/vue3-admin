695af70a6fc4734750078d58a19413eb48741893
刘大卫 <15851990699@163.com>
2022-04-29 11:27:51 +0800

    chore: process.cwd()  Node.js 进程的当前工作目录

--- vite.config.ts
+++ vite.config.ts
@@ -3,18 +3,16 @@ import type { UserConfig, ConfigEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 // import { resolve } from 'path'
 
-// 大概意思 cd __dirname(当前模块的目录名) + cd . + cd dir
+// 大概意思 cd process.cwd()(Node.js 进程的当前工作目录) + cd . + cd dir
 // function pathResolve(dir: string) {
-//   return resolve(__dirname, '.', dir)
+//   return resolve(process.cwd(), '.', dir)
 // }
 
 // https://vitejs.dev/config/
-export default defineConfig(
-  ({}: // command,
-  // mode,
-  ConfigEnv): UserConfig => {
-    return {
-      plugins: [vue()],
-    }
+// {command, mode }:ConfigEnv // 情景配置
+export default defineConfig(({}: ConfigEnv): UserConfig => {
+  // const root = process.cwd(); Node.js 进程的当前工作目录
+  return {
+    plugins: [vue()],
   }
-)
+})
