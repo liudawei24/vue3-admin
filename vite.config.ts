f5130c3547702b3e82932faab51e124b8e1de0d8
刘大卫 <15851990699@163.com>
2022-05-04 09:37:03 +0800

    chore: 新增wrapperEnv

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,16 @@
 import type { UserConfig, ConfigEnv } from 'vite'
 import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
-// import { resolve } from 'path'
+import { resolve } from 'path'
+import { wrapperEnv } from './build/utils'
 
 // 大概意思 cd process.cwd()(Node.js 进程的当前工作目录) + cd . + cd dir
-// function pathResolve(dir: string) {
-//   return resolve(process.cwd(), '.', dir)
-// }
+function pathResolve(dir: string) {
+  return resolve(process.cwd(), '.', dir)
+}
 
 // https://vitejs.dev/config/
-// {command, mode }:ConfigEnv // 情景配置
+// { command, mode }:ConfigEnv // 情景配置
 export default defineConfig(({ command, mode }: ConfigEnv): UserConfig => {
   // Node.js 进程的当前工作目录
   const root = process.cwd()
@@ -20,9 +21,21 @@ export default defineConfig(({ command, mode }: ConfigEnv): UserConfig => {
    * NODE_ENV的值后面的会覆盖前面的。
    */
   const env = loadEnv(mode, root)
+
+  // loadEnv读取的布尔类型是一个字符串。该函数可以转换为布尔类型。
+  const viteEnv = wrapperEnv(env)
+
+  const { VITE_PORT, VITE_PUBLIC_PATH, VITE_PROXY, VITE_DROP_CONSOLE } = viteEnv
+
+  const isBuild = command === 'build'
+
   console.log({
-    command,
-    env,
+    pathResolve,
+    VITE_PORT,
+    VITE_PUBLIC_PATH,
+    VITE_PROXY,
+    VITE_DROP_CONSOLE,
+    isBuild,
   })
   return {
     plugins: [vue()],
