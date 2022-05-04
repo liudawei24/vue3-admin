fc1d83f4f3a99bc19eabdff055079f6d646b40a5
刘大卫 <15851990699@163.com>
2022-05-04 11:30:31 +0800

    chore: 插件:@vitejs/plugin-legacy、@vitejs/plugin-vue-jsx、vite-plugin-html

--- build/vite/plugin/index.ts
+++ build/vite/plugin/index.ts
@@ -1,15 +1,30 @@
 import { PluginOption } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import legacy from '@vitejs/plugin-legacy'
+import { configHtmlPlugin } from './html'
 
 export function createVitePlugins(viteEnv: ViteEnv, isBuild: boolean) {
-  // const {
-  //   VITE_USE_IMAGEMIN,
-  //   VITE_USE_MOCK,
-  //   VITE_LEGACY,
-  //   VITE_BUILD_COMPRESS,
-  //   VITE_BUILD_COMPRESS_DELETE_ORIGIN_FILE,
-  // } = viteEnv
-  console.log(viteEnv, isBuild)
+  const {
+    // VITE_USE_IMAGEMIN,
+    // VITE_USE_MOCK,
+    VITE_LEGACY,
+    // VITE_BUILD_COMPRESS,
+    // VITE_BUILD_COMPRESS_DELETE_ORIGIN_FILE,
+  } = viteEnv
 
-  const vitePlugins: (PluginOption | PluginOption[])[] = []
+  const vitePlugins: (PluginOption | PluginOption[])[] = [
+    // have to
+    vue(),
+    // have to
+    vueJsx(),
+  ]
+
+  // @vitejs/plugin-legacy
+  VITE_LEGACY && isBuild && vitePlugins.push(legacy())
+
+  // vite-plugin-html
+  vitePlugins.push(configHtmlPlugin(viteEnv, isBuild))
+
   return vitePlugins
 }
